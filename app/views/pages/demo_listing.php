b207dc85c1a45333eb8364c8ee09dfe29bbece67
satyajitgh <satyajit@itobuz,com>
2019-07-11 18:32:10 +0530

    add image as background

--- app/views/pages/demo_listing.php
+++ app/views/pages/demo_listing.php
@@ -1,5 +1,6 @@
 <div class="row">
-	<?php 
+
+	<?php
 	$i=1;
 		foreach ($demos as $k => $demo) 
 		{
@@ -11,29 +12,29 @@
 		<div class="col-md-6">
 			<div class="top-bar"><span></span><span></span><span></span> </div>
 			<a class="demo_link" href="<?php echo base_url('showdemo'); ?>" data-id="<?php echo $demo['id']; ?>">
-			<div>
-				<img src="<?php echo site_url('public/assets/demo_images/').$demo['demo_image']; ?>" style="height: ;width:100%">
+			<div class="listing-featuered-image" style="background-image:url(<?php echo site_url('public/assets/demo_images/').$demo['demo_image']; ?>);">
 			</div>
 			<div class="text-center demo-title">
 				<?php echo $demo['demo_name']; ?>				
 			</div>
-			<div class="text-center demo-desc">
-
-			</div>
 			</a>
+			
+			<div class="text-center demo-desc">
+				<?php echo $demo['demo_descriptions'];?>
+			</div>
 		</div>
 		<?php
-	$i++;
-		
-	if($i%2==1){ ?>
-		</div>
-		<?php } ?>
-	<?php
-		}
-		if($i%2==1){
-			echo"</div>";
-		}
-	?>
+			$i++;	
+			if($i%2==1){ ?>
+				</div>
+		<?php
+			}
+
+			}
+			if($i%2==1){
+				echo"</div>";
+			}
+		?>
 
 	<form id="demo-frm" target="_blank" method="post">
 		<input type="hidden" name="demo_id" id="demo_id">
