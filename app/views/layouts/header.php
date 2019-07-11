88dcc2552abcf4c4977a5129c4bd9e3ccda650e8
satyajitgh <satyajit@itobuz,com>
2019-07-11 13:20:29 +0530

    header

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -7,4 +7,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>__PAGE_TITLE__</title>
 	<link href="<?php echo site_url('public/assets/css/'); ?>bootstrap.min.css" rel="stylesheet" id="bootstrap-css">			
 	<link href="<?php echo site_url('public/assets/css/'); ?>main.css" rel="stylesheet" id="bootstrap-css">			
+	<div class="main-header">
+		<div class="container">		
+			<div class="top-header"> 
+				<div class="col-sm-12">
+					<div class="col-sm-6 header-news"> Celebrating 9 years of Success, Grandeur & Togetherness
+					</div>
+
+					<div class="col-sm-6 header-menu top-header-menu"> 
+					<ul id="top-tier-menu" class="">
+					<li id="menu-item-1144" class="menu-item"><a href="http://localhost/itobuzsite/1138-2/">Our Process</a></li>
+					<li id="menu-item-1208" class="menu-item"><a href="#">Our Passion</a></li>
+					</ul>
+					</div>
+				</div>
+			</div>
+			<div class="sec-header"> 
+				<div class="col-sm-12">
+					<div class="col-sm-3 logo"> <img src="http://[::1]/demolisting/public/assets/demo_images/Logo.png">
+					</div>
+
+					<div class="col-sm-9 header-menu main-header-menu"> 
+					<nav class="main-nav">
+						<ul  id="top-tier-menu" class="">
+						<li class="menu-item"><a href="#">Industries</a></li>
+						<li class="menu-item"><a href="#">Services</a></li>
+						<li class="menu-item"><a href="#">Our Process</a></li>
+						<li class="menu-item"><a href="#">About Us</a></li>
+						</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 </head>
+<body>
